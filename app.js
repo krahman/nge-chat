57b2c805011ce8c690c240bbd26611b5465247bf
khal <khal.rahman@gmail.com>
2014-02-24 23:06:43 +0800

    add chat angular controller, add sockjs/socket server

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ var path = require('path');
 
 var app = express();
 
+// start additional sockjs line
+var sockjs = require('sockjs');
+
+var connections = [];
+
+var chat = sockjs.createServer();
+chat.on('connection', function(conn){
+  
+  connections.push(conn);
+  
+  var number = connections.length;
+  conn.write("Welcome, User " + number);
+  conn.on('data', function(message){
+    for(var i=0; i<connections.length; i++){
+      connections[i].write("User " + number + " says: " + message);
+    }
+  });
+  conn.on('close', function(){
+    for(var i=0; i<connections.length; i++){
+      connections[i].write("User " + number + " has disconnected");
+    }
+  });
+});
+
+// end of sockjs line
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +48,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname, '/bower_components'));
 
 // development only
 if ('development' == app.get('env')) {
@@ -31,6 +58,8 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+chat.installHandlers(server, {prefix: '/chat'});
